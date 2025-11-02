4e78d8a0387b71876bb61cf64376573ecabbd5c0
Syrup <goodgamersz665@gmail.com>
2025-11-02 12:54:47 +0000

    fix: use MarkdownIt utils instance for escapeHtml
    - Create mdUtils instance to access escapeHtml independently
    - Fixes build error where md parameter was undefined
    - Removes md parameter from highlightCode function signature

--- helpers/highlight.js
+++ helpers/highlight.js
@@ -1,15 +1,18 @@
 import hljs from 'highlight.js';
+import MarkdownIt from 'markdown-it';
 
-export function highlightCode(str, lang, md) {
+const mdUtils = MarkdownIt().utils;
+
+export function highlightCode(str, lang) {
   if (lang && hljs.getLanguage(lang)) {
     try {
-      const langLabel = `<span class="code-lang">${md.utils.escapeHtml(lang)}</span>`;
-      return `<pre class="hljs" data-lang="${md.utils.escapeHtml(lang)}">` +
+      const langLabel = `<span class="code-lang">${mdUtils.escapeHtml(lang)}</span>`;
+      return `<pre class="hljs" data-lang="${mdUtils.escapeHtml(lang)}">` +
              langLabel +
              `<code>` +
              hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
              '</code></pre>';
     } catch (__) {}
   }
-  return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+  return '<pre class="hljs"><code>' + mdUtils.escapeHtml(str) + '</code></pre>';
 }
